27f65c275ee86d319cba5bcdd24ac4ff972d6006
capoo <flange0322@gmail.com>
2019-04-22 10:41:32 +0800

    First Commit

---
+++ SwiftClass03Tests/SwiftClass03Tests.swift
@@ -0,0 +1,34 @@
+//
+//  SwiftClass03Tests.swift
+//  SwiftClass03Tests
+//
+//  Created by L20102-02 on 2019/4/22.
+//  Copyright © 2019年 L20102-02. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftClass03
+
+class SwiftClass03Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
